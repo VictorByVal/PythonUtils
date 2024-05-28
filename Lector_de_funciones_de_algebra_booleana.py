1f43789efbd7e2dedcfb9b916efb136e6c0f3f07
Victor Gabriel <78563949+VictorByVal@users.noreply.github.com>
2024-05-28 17:36:35 -0600

    Delete Lector_de_funciones_de_algebra_booleana.py

--- Lector_de_funciones_de_algebra_booleana.py
+++
@@ -1,8 +0,0 @@
-# Idea: Desarrollar un lector de funciones de algebra booleana 
-
-def definir_variables(variables):
-    for variable in variables:
-        print(variables)
-
-# Idea de comando para emplear 
-definir_variables(["x", "y", "z"])
